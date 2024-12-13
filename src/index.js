d20a039b93eadb1c21573ee75ad075c76287d4d5
karenvincent <karenlynnvincent@gmail.com>
2024-12-13 11:29:41 -0800

    Added API search functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "00c5f44786367a1bt6a05fad239ccoe5";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
+}
+
+function handleError(error) {
+  console.error("Error fetching weather data:", error);
+  alert("Could not fetch weather data. Please try again.");
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Chicago");
